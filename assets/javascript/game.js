63b3c389fccc6f1ef9e1172d4a7af3691d6c4b15
marclevetin <marc@derbyhat.org>
2017-09-27 22:29:42 -0400

    got random letter selected at start of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,19 @@
+
+const totalPlayerWins = 0
+const totalPlayerLosses = 0
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const guessedLetters = ""
+const remainingCountOfPlayerGuesses = 10
+let correctLetter = ""
+
 function startGame() {
   console.log("Game started")
+  commitCorrectLetter()
+  console.log(correctLetter)
+}
+
+function commitCorrectLetter() {
+  let randomNumber = Math.floor(Math.random() * 26)
+  correctLetter = alphabet[randomNumber]
+  return correctLetter
 }
