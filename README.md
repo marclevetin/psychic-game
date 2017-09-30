78199d6e7be66d25e2bbbf9759b5fec6802668da
marclevetin <marc@derbyhat.org>
2017-09-29 21:01:24 -0400

    refactored code to a text based MVP.  Completed basic JS.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # psychic-game
-This is Trilogy Ed homework: psychic game.
+This is Trilogy Ed homework: psychic game.  It's an experiment written in vanilla JavaScript.
 
 ## rules and requirements
 1. The app randomly picks a letter, and the user has to guess which letter the app chose. Put the following text on your page:
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,48 @@
+// page setup
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+let correctLetter = "";
+commitCorrectLetter();
+let guessedLetters = "";
+let remainingCountOfPlayerGuesses = 10;
+let totalPlayerWins = 0;
+let totalPlayerLosses = 0;
 
-const totalPlayerWins = 0
-const totalPlayerLosses = 0
-const alphabet = "abcdefghijklmnopqrstuvwxyz"
-const guessedLetters = ""
-const remainingCountOfPlayerGuesses = 10
-let correctLetter = ""
+// updates when a key is pressed
+document.onkeypress = function(key) {
+  guessedLetters += key.key
+  remainingCountOfPlayerGuesses -= 1
+  isGameOver(key.key)
+  updateDOM()
+}
 
-function startGame() {
-  console.log("Game started")
+function updateDOM() {
+  document.getElementById("guessedletters").innerHTML = guessedLetters;
+  document.getElementById("remaining-guesses").innerHTML = remainingCountOfPlayerGuesses;
+  document.getElementById("totalplayerwins").innerHTML = totalPlayerWins;
+  document.getElementById("totalplayerlosses").innerHTML = totalPlayerLosses;
+}
+
+function isGameOver(letter) {
+  if (letter == correctLetter) {
+    totalPlayerWins += 1
+    alert('You won!')
+    resetGame()
+  } else if (remainingCountOfPlayerGuesses < 1 && letter != correctLetter) {
+    totalPlayerLosses += 1
+    alert('You lost.  Try again.')
+    resetGame()
+  }
+}
+
+function resetGame() {
+  guessedLetters = "";
+  remainingCountOfPlayerGuesses = 10;
   commitCorrectLetter()
-  console.log(correctLetter)
 }
 
 function commitCorrectLetter() {
-  let randomNumber = Math.floor(Math.random() * 26)
-  correctLetter = alphabet[randomNumber]
+  let randomNumber = Math.floor(Math.random() * 26);
+  correctLetter = alphabet[randomNumber];
+  console.log(correctLetter)
   return correctLetter
 }
